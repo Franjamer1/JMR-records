888d9eadb0c47583da4c385fce522da24cbd1582
Francisco Patiño <franjamer1@gmail.com>
2025-10-23 14:01:00 -0300

    requireRole applied to routes

--- back/src/routes/appointmentsRouter.ts
+++ back/src/routes/appointmentsRouter.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { getAllAppointment, getAppointmentByid, newAppointment, cancelAppointment } from "../controllers/appointmentsController";
 import { authMiddleware } from "../middlewares/authMiddleware";
+import { requireRole } from "../middlewares/roleMiddleware";
 
 const appointmentRouter = Router();
 
-appointmentRouter.get("/", authMiddleware, getAllAppointment);
-appointmentRouter.get("/:turnId", authMiddleware, getAppointmentByid);
-appointmentRouter.post("/schedule", authMiddleware, newAppointment);
-appointmentRouter.put("/cancel/:turnId", authMiddleware, cancelAppointment);
+appointmentRouter.get("/", authMiddleware, requireRole(["ADMIN"]), getAllAppointment);
+appointmentRouter.get("/:turnId", authMiddleware, requireRole(["USER", "ADMIN"]), getAppointmentByid);
+appointmentRouter.post("/schedule", authMiddleware, requireRole(["USER", "ADMIN"]), newAppointment);
+appointmentRouter.put("/cancel/:turnId", authMiddleware, requireRole(["USER", "ADMIN"]), cancelAppointment);
 
 export default appointmentRouter;
--- back/src/routes/userRouter.ts
+++ back/src/routes/userRouter.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { createUser, getAllUsers, getUserById, loginUser } from "../controllers/userController";
 import { authMiddleware } from "../middlewares/authMiddleware";
+import { requireRole } from "../middlewares/roleMiddleware";
 // import { register } from "module";
 
 const usersRouter = Router();
 
-usersRouter.get("/", authMiddleware, getAllUsers);
-usersRouter.get("/:id", authMiddleware, getUserById);
+usersRouter.get("/", authMiddleware, requireRole(["ADMIN"]), getAllUsers);
+usersRouter.get("/:id", authMiddleware, requireRole(["ADMIN"]), getUserById);
 usersRouter.post("/register", createUser);
 usersRouter.post("/login", loginUser);
 
